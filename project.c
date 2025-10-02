b6132bef14aa0601f03a2b2c4cfc4a742e7e7696
Enockbwalya104 <enockbwalya104@gmail.com>
2025-10-02 09:37:40 -0700

    Dat1: Temperature coverter

---
+++ project.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+
+    printf(" 250 ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+    printf(" 99.0 %.1f\n\n", fahrenheit);
+
+    printf(" 99 ");
+    scanf("%f", &fahrenheit);
+
+    celsius = (fahrenheit - 32) * 5 / 9;
+    printf("250.0 %.1f\n", celsius);
+
+    return 0;
+}
