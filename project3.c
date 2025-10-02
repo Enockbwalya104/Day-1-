e99bde474c7ce6c2aab73ba8631b305bfe425ee6
Enockbwalya104 <enockbwalya104@gmail.com>
2025-10-01 22:18:21 -0700

    Day1: Anrithmetic operations on two numbers

---
+++ project3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+  int main() {
+
+a = float(input(" 5"));
+b = float(input(" 7 "));
+
+
+sum = a + b;
+difference = a - b;
+product = a * b;
+
+if b != 0:
+    quotient = a / b;
+else:
+    quotient = None;
+
+
+print("Sum:", _sum);
+print("Difference (a − b):", difference);
+print("Product:", product);
+if quotient is not None:
+    print("Quotient (a ÷ b):", quotient);
+else:
+    print("Quotient: undefined (division by zero)");
+
+ return0; 
+
+}
