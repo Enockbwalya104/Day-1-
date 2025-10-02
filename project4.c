0e5a9a7bc978a6dc13047975f485d16f69bc52ca
Enockbwalya104 <enockbwalya104@gmail.com>
2025-10-02 10:03:03 -0700

    Day1: Temperature converter

--- project4.c
+++ project4.c
@@ -1,37 +1,21 @@
 #include <stdio.h>
- 
+
 int main() {
-    int choice;
-    float temp, result;
+    float celsius, fahrenheit;
 
 
-float celsius_to_fahrenheit(float celsius) {
-    return (celsius * 9.0 / 5.0) + 32;
-}
+    printf("  250 ");
+    scanf("%f", &celsius);
 
-float fahrenheit_to_celsius(float fahrenheit) {
-    return (fahrenheit - 32) * 5.0 / 9.0;
-}
+    fahrenheit = (celsius * 9 / 5) + 32;
+    printf("99.0 %.1f\n\n", fahrenheit);
 
-    printf("Temperature Converter\n");
-    printf("1. Celsius to Fahrenheit\n");
-    printf("2. Fahrenheit to Celsius\n");
-    printf("Enter your choice (1 or 2): ");
-    scanf("%d", &choice);
+    printf(" 99 ");
+    scanf("%f", &fahrenheit);
 
-    if (choice == 1) {
-        printf("Enter temperature in Celsius: ");
-        scanf("%f", &temp);
-        result = celsius_to_fahrenheit(temp);
-        printf("%.2f°C is equal to %.2f°F\n", temp, result);
-    } else if (choice == 2) {
-        printf("Enter temperature in Fahrenheit: ");
-        scanf("%f", &temp);
-        result = fahrenheit_to_celsius(temp);
-        printf("%.2f°F is equal to %.2f°C\n", temp, result);
-    } else {
-        printf("Invalid choice. Please enter 1 or 2.\n");
-    }
+    celsius = (fahrenheit - 32) * 5 / 9;
+    printf("250.0%.1f\n", celsius);
 
     return 0;
 }
+
